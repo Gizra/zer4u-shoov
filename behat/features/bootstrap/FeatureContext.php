0210de22feaf25e27b8b5a50f602221dc124b6ac
Amitaibu <amitai@gizra.com>
2015-07-26 14:57:01 +0300

    Add behat tests to add to cart

--- behat/features/bootstrap/FeatureContext.php
+++ behat/features/bootstrap/FeatureContext.php
@@ -12,27 +12,156 @@ class FeatureContext extends MinkContext implements SnippetAcceptingContext {
    * @Given I am an anonymous user
    */
   public function iAmAnAnonymousUser() {
-    // Just let this pass-through.
+    // Just let this passthru.
   }
 
   /**
-   * @When I visit the homepage
+   * @When I select item and add to cart
    */
-  public function iVisitTheHomepage() {
-    $this->getSession()->visit($this->locatePath('/'));
+  public function iSelectItemAndAddToCart() {
+    // Wait for the "Add to cart" button to appear.
+    $this->iWaitForCssElement('#addCart');
+
+    // Add to cart.
+    $element = $this->getSession()->getPage()->find('css', '#addCart img');
+    $element->click();
   }
 
   /**
-   * @Then I should have access to the page
+   * @When I add my personal info
    */
-  public function iShouldHaveAccessToThePage() {
-    $this->assertSession()->statusCodeEquals('200');
+  public function iAddMyPersonalInfo() {
+    // Wait for the "Personal info" overlay to appear.
+    $this->iWaitForCssElement('#fbBoxLiner');
+    $this->getSession()->switchToIFrame('fbContent');
+
+    $element = $this->getSession()->getPage()->find('css', '#RadDatePicker1_popupButton');
+    $element->click();
+
+    // Go to next dates.
+    $element = $this->getSession()->getPage()->find('css', '.rcFastNext');
+    $element->click();
+    $element->click();
+
+    // Select first enabled date.
+    $element = $this->getSession()->getPage()->find('css', 'td.rcOtherMonth');
+    $element->click();
+
+    $element = $this->getSession()->getPage()->find('css', '#searchbox2');
+    $element->setValue('תל אביב-יפו');
+
+    $element = $this->getSession()->getPage()->find('css', '#searchbox3_Input');
+    $element->click();
+
+    $this->iWaitForCssElement('#searchbox3_DropDown li.rcbItem');
+
+
+    // Get the 3rd element, as clicking on the first one seems to click the
+    // wrong element and cause an error.
+    $element = $this->getSession()->getPage()->find('css', '#searchbox3_DropDown .rcbItem:nth-child(3)');
+    $element->click();
+
+
+    $element = $this->getSession()->getPage()->find('css', '#HOMENUMBER');
+    $element->setValue('5');
+
+
+    $element = $this->getSession()->getPage()->find('css', '#SetSessionButton');
+    $element->click();
+
+
+    // We can simply reload the page.
+    $this->getSession()->wait(2000);
+    $this->getSession()->reload();
+
   }
 
   /**
-   * @Then I should not have access to the page
+   * @Then I should see the item added to the cart
    */
-  public function iShouldNotHaveAccessToThePage() {
-    $this->assertSession()->statusCodeEquals('403');
+  public function iShouldSeeTheItemAddedToTheCart() {
+    $this->iWaitForCssElement('#iframecart');
+
+    $this->getSession()->switchToIFrame('iframecart');
+    $this->waitFor(function($context) {
+      try {
+        if (!$element = $context->getSession()->getPage()->find('css', '.cart_bg')) {
+          return FALSE;
+        }
+        return $element->getText() == '1';
+      }
+      catch (WebDriver\Exception $e) {
+        if ($e->getCode() == WebDriver\Exception::NO_SUCH_ELEMENT) {
+          return FALSE;
+        }
+        throw $e;
+      }
+    });
+  }
+
+  /**
+   * @Then I should wait for the text :arg1 to :arg2
+   */
+  public function iShouldWaitForTheTextTo($text, $appear) {
+    $this->waitForXpathNode(".//*[contains(normalize-space(string(text())), \"$text\")]", $appear == 'appear');
+  }
+
+
+  /**
+   * @Then /^I wait for css element "([^"]*)" to "([^"]*)"$/
+   */
+  public function iWaitForCssElement($element, $appear = 'appear') {
+    $xpath = $this->getSession()->getSelectorsHandler()->selectorToXpath('css', $element);
+    $this->waitForXpathNode($xpath, $appear == 'appear');
+  }
+
+
+  /**
+   * Helper function; Execute a function until it return TRUE or timeouts.
+   *
+   * @param $fn
+   *   A callable to invoke.
+   * @param int $timeout
+   *   The timeout period. Defaults to 15 seconds.
+   *
+   * @throws Exception
+   */
+  private function waitFor($fn, $timeout = 15000) {
+    $start = microtime(true);
+    $end = $start + $timeout / 1000.0;
+    while (microtime(true) < $end) {
+      if ($fn($this)) {
+        return;
+      }
+    }
+    throw new \Exception('waitFor timed out.');
+  }
+  /**
+   * Wait for an element by its XPath to appear or disappear.
+   *
+   * @param string $xpath
+   *   The XPath string.
+   * @param bool $appear
+   *   Determine if element should appear. Defaults to TRUE.
+   *
+   * @throws Exception
+   */
+  private function waitForXpathNode($xpath, $appear = TRUE) {
+    $this->waitFor(function($context) use ($xpath, $appear) {
+        try {
+          $nodes = $context->getSession()->getDriver()->find($xpath);
+          if (count($nodes) > 0) {
+            $visible = $nodes[0]->isVisible();
+            return $appear ? $visible : !$visible;
+          }
+          return !$appear;
+        }
+        catch (WebDriver\Exception $e) {
+          if ($e->getCode() == WebDriver\Exception::NO_SUCH_ELEMENT) {
+            return !$appear;
+          }
+          throw $e;
+        }
+      });
   }
 }
---
+++ behat/features/cart.feature
@@ -0,0 +1,12 @@
+Feature: Add to cart
+  In order to be able to purchase items
+  As an anonymous user
+  We need to be able to to add items to cart
+
+  @javascript
+  Scenario: Add item to cart
+    Given I am an anonymous user
+    When  I visit "page_18764"
+    And   I select item and add to cart
+    And   I add my personal info
+    Then  I should see the item added to the cart
